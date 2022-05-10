57095af7a0e52633628d1d9023253eaa076421fb
Jake <castiglianoj@my.lanecc.edu>
2022-05-10 12:48:45 -0700

    Eliza Done

---
+++ Midterm_Core5/Eliza/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Eliza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userStatement = Console.ReadLine();
+            Console.WriteLine(CreateElizaResponse(userStatement));
+            
+        }
+
+        static string CreateElizaResponse(string userIn)
+        {
+            if (userIn.Contains(" my ") && (userIn.Contains(" hate ") || userIn.Contains(" love ")))
+            {
+                int myIndex = userIn.IndexOf("my ");
+                string nounHopefully = userIn.Substring(myIndex + 3);
+                string elizaResponse = "You have strong feelings about your " + nounHopefully + ". Can you tell me more about your " + nounHopefully + "?";
+                return elizaResponse;
+            }
+            else if (userIn.Contains(" my "))
+            {
+                int myIndex = userIn.IndexOf("my ");
+                string nounHopefully = userIn.Substring(myIndex + 3);
+                string elizaResponse = "Can you tell me more about your " + nounHopefully + "?";
+                return elizaResponse;
+            }
+            else if (userIn.Contains(" hate ") || userIn.Contains(" love "))
+            {
+                string elizaResponse = "You have strong feelings about that!";
+                    return elizaResponse;
+            }
+            else
+            {
+                string elizaResponse = "Please go on.";
+                    return elizaResponse;
+            }
+        }
+    }
+}
---
+++ Midterm_Core5/Palindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void CreateElizaResponse(string userIn)
+        {
+            
+        }
+
+        
+    }
+
+
+}
